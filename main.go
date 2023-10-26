6aa2167ec3610ed71daa371f294f3a43d6a9a0f4
chenqingsong <2895100818@qq.com>
2023-10-26 15:46:15 +0800

    daily003

--- main.go
+++ main.go
@@ -2,7 +2,30 @@ package main
 
 import "fmt"
 
-
 func main() {
-	fmt.Println("hello word")
+	//cardGame()
+	var cards = []string{"complete", newCard()}
+	cards = append(cards, "combination")
+
+	//fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func cardGame() {
+	//var card string = "Ace of Spades"
+	// card := "Ace of Spades"
+	// card = "Two of Spades"
+	card := newCard()
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "diamonds"
+}
+
+func intCard() int {
+	return 45
 }
