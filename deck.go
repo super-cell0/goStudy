89ed405f2dd16f1a09143c1549d36cb5cc62b79e
chenqingsong <2895100818@qq.com>
2023-10-26 17:07:49 +0800

    p14

--- deck.go
+++ deck.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+type deck []string
+
+// 打印循环
+func (index deck) printDeck() {
+	for i, card := range index {
+		fmt.Println(i, card)
+	}
+
+}
--- main.go
+++ main.go
@@ -3,19 +3,68 @@ package main
 import "fmt"
 
 func main() {
-	//cardGame()
-	var cards = []string{"complete", newCard()}
-	cards = append(cards, "combination")
 
-	//fmt.Println(cards)
+	//var cards = deck{"complete", newCard()}
+	//cards = append(cards, "combination")
+	//for循环打印
+	//for i, card := range cards {
+	//	fmt.Println(i, card)
+	//}
+	//cards.printDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
+	//name, age := getNameAndAge()
+	//fmt.Println(name)
+	//fmt.Println(age)
+
+	demoFor()
+
+}
+
+func demoFor() {
+	var count = 10
+	for i := 0; i < count; i++ {
+		println(i)
 	}
 }
 
+func demoBool() {
+	var age = 18
+	if age >= 18 {
+		fmt.Println("yes")
+	} else {
+		fmt.Println("no")
+	}
+}
+
+func createArray() {
+	var names = []int{1, 2, 3}
+	for i, name := range names {
+		println(i, name)
+	}
+}
+
+func demoIota() {
+	//iota是一个预定义的标识符，用于创建枚举常量序列。
+	//当定义一个枚举类型的常量时，iota会被重置为0，然后每个连续的常量声明都会使iota递增1
+	const (
+		a1 = iota
+		a2 = iota
+		a3 = iota
+		_
+		a4 = iota
+	)
+
+	fmt.Println(a1, a2, a3, a4)
+}
+
+func constStatement() {
+	const PI float64 = 3.1415
+
+	fmt.Println(PI)
+}
+
 func cardGame() {
-	//var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
 	// card := "Ace of Spades"
 	// card = "Two of Spades"
 	card := newCard()
@@ -28,4 +77,26 @@ func newCard() string {
 
 func intCard() int {
 	return 45
+}
+
+// 变量批量声明
+func varStatement() {
+	var (
+		name   string
+		age    int
+		gender string
+	)
+
+	name = "chen"
+	age = 24
+	gender = "男"
+
+	fmt.Println(name, age, gender)
+
+	//还有类型推断
+	//短变量声明 := 只能放在函数内部
+}
+
+func getNameAndAge() (string, int) {
+	return "chen", 24
 }
